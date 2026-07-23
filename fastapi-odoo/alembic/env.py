102b14f283996b1aeb6f1ebe8c6e9631fc0eb950
alireza Sharzeh <alirezasharzeh@gmail.com>
2026-07-24 01:51:20 +0330

    initialize odoo with eCommerce module(no setup action needed)

--- fastapi-odoo/alembic/env.py
+++ fastapi-odoo/alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import engine_from_config, pool
 
 from app.core.config import settings
 from app.core.database import Base
-from app.models import Contact, Item, Product, SaleOrder, SaleOrderLine  # noqa: F401
+from app.models import Contact, Product, SaleOrder, SaleOrderLine  # noqa: F401
 
 config = context.config
 config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
