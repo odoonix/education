3fbfb38377dca50d0ccf688932db6c1312b59dc7
esmaeil taheri <esi.taheri@yahoo.com>
2026-07-23 16:42:06 +0330

    chore: add repoistory abstract classes to __init__

---
+++ app/domain/repositories/__init__.py
@@ -0,0 +1,12 @@
+from .contact_repository import ContactRepository
+from .product_repository import ProductRepository
+from .sale_order_repository import SaleOrderRepository
+from .sale_order_line_repository import SaleOrderLineRepository
+
+
+__all__ = [
+    "ContactRepository",
+    "ProductRepository",
+    "SaleOrderRepository",
+    "SaleOrderLineRepository",
+]
